03c32a20d3e22d221ee4dbc67f2ef595c4e8ac8c
Vinicius Pickrodt <vinipicrkodt@gmail.com>
2021-04-12 16:29:05 +0200

    kafka test scripts

---
+++ install_kafka.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt-get install openjdk-8-jdk -y
+mkdir kafka
+cd kafka
+wget https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
+tar -xzf kafka_2.13-2.7.0.tgz
+cd kafka_2.13-2.7.0
+
---
+++ start_kafka.sh
@@ -0,0 +1,3 @@
+nohup sudo bin/zookeeper-server-start.sh config/zookeeper.properties &
+nohup sudo bin/kafka-server-start.sh config/server.properties &
+
